ec160e3bd93d04e6313aa75d73edd5dc3288595d
Nana-Taran <nt.kvadrant@gmail.com>
2025-08-28 11:43:22 +0200

    task 3 updated

--- src/main/java/task3/Task3.java
+++ src/main/java/task3/Task3.java
@@ -5,5 +5,9 @@ public class Task3 {
     public static void main(String[] args) {
 
         System.out.println("Код задачи 3");
+
+        System.out.println("Код задачи 3 доработан");
+
+
     }
 }
